68a17159d0b83368f14fdd1cd8940a5a95994165
gonmastronardi <gonmastronardi@192.168.1.100>
2018-05-27 22:04:36 -0300

    OAuth2

--- src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/HistoriaclinicaApplication.java
+++ src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/HistoriaclinicaApplication.java
@@ -2,8 +2,11 @@ package ar.edu.unlp.pasae.pasaetrabajofinalbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 
 @SpringBootApplication
+@EnableOAuth2Sso
+
 public class HistoriaclinicaApplication {
 
 	public static void main(String[] args) {
