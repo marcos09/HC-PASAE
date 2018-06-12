26426883766b42350e62b02c49b0a715ce47bb66
gonmastronardi <gonmastronardi@192.168.0.11>
2018-06-12 17:20:24 -0300

    Alta y baja de ingresos. No funciona por problemas de permisos. Error 403 Forbidden

--- src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/controller/IngresoPacienteController.java
+++ src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/controller/IngresoPacienteController.java
@@ -3,13 +3,19 @@ package ar.edu.unlp.pasae.pasaetrabajofinalbackend.controller;
 import java.util.Collection;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.edu.unlp.pasae.pasaetrabajofinalbackend.dto.GenericDTO;
+import ar.edu.unlp.pasae.pasaetrabajofinalbackend.dto.IngresoPacienteDTO;
 import ar.edu.unlp.pasae.pasaetrabajofinalbackend.services.IngresoPacienteService;
 
 @RestController
@@ -36,6 +42,17 @@ public class IngresoPacienteController {
 	    return this.getIngresoService().retrive(id);
 	   }
 	 
+	 //Elimino ingreso con el id
+	 @DeleteMapping(path = "/{id}")
+	   public void delete(@PathVariable(value = "id") Long id) {
+	     this.getIngresoService().delete(id);
+	   }
+	 
+	 //Alta de ingreso
+	 @PutMapping(path = "/createIngreso", consumes = "application/json", produces = "application/json")
+	   public  void create(@RequestBody @Valid IngresoPacienteDTO ingresoPaciente) {
+	        this.getIngresoService().create(ingresoPaciente);
+	   }
 
 	private IngresoPacienteService getIngresoService() {
 		return ingresoService;
