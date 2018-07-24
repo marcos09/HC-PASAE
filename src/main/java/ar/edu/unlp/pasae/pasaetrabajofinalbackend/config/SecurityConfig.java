f959af52d99d8fc9584223ff7ee4678e4ea6b5dc
gonmastronardi <gonmastronardi@192.168.0.13>
2018-07-24 17:45:25 -0300

    Configuracion Spring para rutas Angular

--- src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/config/SecurityConfig.java
+++ src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/config/SecurityConfig.java
@@ -1,22 +1,46 @@
 package ar.edu.unlp.pasae.pasaetrabajofinalbackend.config;
 
+import java.io.IOException;
 import java.util.Arrays;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 @EnableOAuth2Sso
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Lazy
+public class SecurityConfig extends WebSecurityConfigurerAdapter  implements WebMvcConfigurer{
 	
+	@Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+      registry.addResourceHandler("/**/*")
+        .addResourceLocations("classpath:/static/")
+        .resourceChain(true)
+        .addResolver(new PathResourceResolver() {
+            @Override
+            protected Resource getResource(String resourcePath,
+                Resource location) throws IOException {
+                Resource requestedResource = location.createRelative(resourcePath);
+                return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                : new ClassPathResource("/static/index.html");
+            }
+        });
+    }
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -35,6 +59,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().disable()
 			.headers().frameOptions().disable();
 	}
+	
+	
 	
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
