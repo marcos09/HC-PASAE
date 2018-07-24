5b8c652831753010292fcdb771eb9752777f7cbc
Marcos Matias Bosnic <marcos@MacBook-Pro-de-Marcos.local>
2018-07-24 12:53:43 -0300

    Fix Patologia transform

--- src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/controller/IngresoPacienteController.java
+++ src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/controller/IngresoPacienteController.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -67,6 +68,7 @@ public class IngresoPacienteController {
 	}
 
 	// Alta de ingreso
+	@CrossOrigin
 	@PutMapping(path = "/createIngreso", consumes = "application/json", produces = "application/json")
 	public void create(@RequestBody @Valid IngresoPacienteDTO ingresoPaciente) {
 		this.getIngresoService().create(ingresoPaciente);
--- src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/transform/PatologiaTransformer.java
+++ src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/transform/PatologiaTransformer.java
@@ -15,24 +15,24 @@ import ar.edu.unlp.pasae.pasaetrabajofinalbackend.entity.Patologia;
 public class PatologiaTransformer implements Transformer<Patologia, PatologiaDTO> {
 
 	public PatologiaTransformer() {
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public PatologiaDTO toDTO(Patologia e) {
-		// TODO Auto-generated method stub
-		return new PatologiaDTO(e.getNombre(),e.getOtroDato(),this.toDTO(e.getFather()),this.toSetDTO(e.getChilds()));
+		if(e != null)
+			return new PatologiaDTO(e.getNombre(),e.getOtroDato(),this.toDTO(e.getFather()),this.toSetDTO(e.getChilds()));
+		return null;
 	}
 
 	@Override
 	public Patologia toEntity(PatologiaDTO dto) {
-		// TODO Auto-generated method stub
-		return new Patologia(dto.getId(),this.toEntity(dto.getFather()),this.toListEntity(dto.getChilds()),dto.getNombre(),dto.getOtroDato());
+		if(dto != null)
+			return new Patologia(dto.getId(),this.toEntity(dto.getFather()),this.toListEntity(dto.getChilds()),dto.getNombre(),dto.getOtroDato());
+		return null;
 	}
 
 	@Override
 	public List<PatologiaDTO> toListDTO(List<Patologia> list) {
-		// TODO Auto-generated method stub
 		List<PatologiaDTO> lista = new ArrayList<PatologiaDTO>();
 		for (Patologia p : list) {
 			lista.add(this.toDTO(p));
@@ -42,7 +42,6 @@ public class PatologiaTransformer implements Transformer<Patologia, PatologiaDTO
 
 	@Override
 	public Collection<PatologiaDTO> toCollectionDTO(List<Patologia> list) {
-		// TODO Auto-generated method stub
 		Collection<PatologiaDTO> lista = new ArrayList<PatologiaDTO>();
 		for (Patologia p : list) {
 			lista.add(this.toDTO(p));
@@ -52,7 +51,6 @@ public class PatologiaTransformer implements Transformer<Patologia, PatologiaDTO
 
 	@Override
 	public Set<Patologia> toListEntity(Set<PatologiaDTO> list) {
-		// TODO Auto-generated method stub
 		Set<Patologia> lista = new HashSet<Patologia>();
 		for (PatologiaDTO p : list) {
 			lista.add(this.toEntity(p));
@@ -61,7 +59,6 @@ public class PatologiaTransformer implements Transformer<Patologia, PatologiaDTO
 	}
 
 	public Set<PatologiaDTO> toSetDTO(Set<Patologia> list) {
-		// TODO Auto-generated method stub
 		Set<PatologiaDTO> lista = new HashSet<PatologiaDTO>();
 		for (Patologia p : list) {
 			lista.add(this.toDTO(p));
