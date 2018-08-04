83dbe28d1e5ee3e69b1c5387e7b6d72fcf83d3aa
gonmastronardi <gonmastronardi@172.16.144.64>
2018-08-03 22:17:33 -0300

    Faltaba agregar PUT en los metodos permitidos para CORS

--- src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/config/SimpleCorsFilter.java
+++ src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/config/SimpleCorsFilter.java
@@ -30,7 +30,7 @@ public class SimpleCorsFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
 				"access-control-allow-headers,access-control-allow-methods,access-control-allow-origin,authorization,content-type");
