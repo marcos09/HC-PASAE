3d23b58247988abd3fc2dd6323caf8427df4d6f2
gonmastronardi <gonmastronardi@192.168.1.107>
2018-09-09 17:31:04 -0300

    Update egreso

--- src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/services/EstudioComplementarioService.java
+++ src/main/java/ar/edu/unlp/pasae/pasaetrabajofinalbackend/services/EstudioComplementarioService.java
@@ -3,12 +3,13 @@ package ar.edu.unlp.pasae.pasaetrabajofinalbackend.services;
 import java.util.List;
 
 import ar.edu.unlp.pasae.pasaetrabajofinalbackend.dto.EstudioComplementarioDTO;
+import ar.edu.unlp.pasae.pasaetrabajofinalbackend.exception.BaseException;
 
 public interface EstudioComplementarioService {
 	
 	void create(EstudioComplementarioDTO persistentDTO);
 
-	void update(EstudioComplementarioDTO persistentDTO);
+	void update(EstudioComplementarioDTO persistentDTO) throws BaseException;
 
 	void delete(Long id);
 
